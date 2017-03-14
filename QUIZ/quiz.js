f4621e5789be6247af34cb3c175d9d2e1b89a573
SteveKinseyWHC <steve.kinsey@whitehallcollege.com>
2017-03-14 14:29:46 +0000

    Add files via upload

---
+++ QUIZ/quiz.js
@@ -0,0 +1,24 @@
+var questionNumber = 1;
+var score = 0;
+
+function showQuestion(question) {
+	$('.questionScreen').hide();
+    $('.finishedScreen').hide();
+	if ($('.questionScreen:nth-of-type('+ question + ')').length > 0) {
+		$('.questionScreen:nth-of-type('+ question + ')').show();
+	} else {
+		$('#finishedScreen').show();
+	}
+}
+
+showQuestion(questionNumber);
+
+$('span').click(function(){
+	if ($(this).hasClass('correct')) {
+	score++;
+	$('#scoreTally').html(score);
+	}   
+	questionNumber++;
+	showQuestion(questionNumber);	 
+});	
+
